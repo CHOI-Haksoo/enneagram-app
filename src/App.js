02b37be1f7071145c05e9554f68ddc501d5a8f97
Haksoo Choi <ninegram54@gmail.com>
2025-06-11 13:40:56 +0900

    Fix: set MainScreen as default route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // App.js
 import React from "react";
-import BoxSelector from "./BoxSelector"; // BoxSelector 불러오기
+import QuestionContainer from "./TestContainer";
+// 필요한 다른 컴포넌트는 상황에 따라 import 추가 가능
+// import BoxSelector from "./BoxSelector";
 
 function App() {
   return (
     <div className="App">
-      <BoxSelector /> {/* 지금은 BoxSelector만 표시하도록 */}
+      <QuestionContainer />
     </div>
   );
 }
